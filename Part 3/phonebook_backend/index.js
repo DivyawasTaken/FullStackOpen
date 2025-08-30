a8b72bd43388ca39a75fd990cd2170d0a8a4197b
Desktop <112158077+DivyawasTaken@users.noreply.github.com>
2025-08-30 17:17:01 +0800

    update

--- Part 3/phonebook_backend/index.js
+++ Part 3/phonebook_backend/index.js
@@ -3,6 +3,7 @@ const app = express()
 // var morgan = require('morgan')
 
 app.use(express.json())
+app.use(express.static('dist'))
 
 // morgan.token('post', function (tokens, req, res) { 
 //     if (tokens.method(req,res) === 'POST') {
